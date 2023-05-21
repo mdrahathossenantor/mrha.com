e7d10539b6105cf02b8228bdf69b45f522e4564a
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2023-05-22 01:58:03 +0600

    version mountain

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("version 2.0");
+console.clear();
+console.log("version 2.0 mountains");
+
+// Creating a digital clock
+let span = document.getElementById("time")
+
+function formateTime(time) {
+    if (time < 10) {
+        time = `0${time}`
+    }
+    return time
+}
+
+function showTime() {
+    const times = new Date()
+    let hours = formateTime(times.getHours())
+    let minutes = formateTime(times.getMinutes())
+    let seconds = formateTime(times.getSeconds())
+    let clock = `${hours}:${minutes}:${seconds}`
+    span.innerHTML = clock
+}
+showTime()
+window.setInterval(showTime, 1000)
